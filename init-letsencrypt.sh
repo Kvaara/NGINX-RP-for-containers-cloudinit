440b299d2b516e41487d699588265897ebc9de4e
Kvaara <karnavaara.jesse@gmail.com>
2023-03-11 20:35:01 +0200

    Okay... Moment of truth... Now I should get the trusted ca certifications and TLS/SSL should work properly in Jenkins, Sonarqube, and Gitlab.

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(dev.codebased.fi www.dev.codebased.fi)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="jesse.karnavaara@codebased.fi" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
